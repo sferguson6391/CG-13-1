50c390a3ba553e2721b32e6283720c0d8fa62242
Sarah <sferguson6391@gmail.com>
2018-10-03 18:15:27 -0500

    returns sum of even numbers in array

---
+++ NumberUtility/NumberUtility/NumberCruncher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberUtility
+{
+        public static class NumberCruncher
+        {
+            public static int EvenNumbers(List<int> numberList)
+            {
+            int sum = 0;
+                foreach (int number in numberList)
+                {
+                if (number % 2 == 0)
+                    sum += number;
+                }
+            return sum;
+            }
+        }
+}
