98cb2222eddae6c0a5226d4562b7a50e096c5a99
Sarah <sferguson6391@gmail.com>
2018-10-03 18:20:56 -0500

    more numbers in array

--- CG 13-1/CG 13-1/Program.cs
+++ CG 13-1/CG 13-1/Program.cs
@@ -8,7 +8,7 @@ namespace CG_13_1
     {
         static void Main(string[] args)
         {
-            List<int> numberList = new List<int>() { 2, 3, 4 };
+            List<int> numberList = new List<int>() { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
             Console.Write(NumberCruncher.EvenNumbers(numberList));
             Console.ReadLine();
         }
